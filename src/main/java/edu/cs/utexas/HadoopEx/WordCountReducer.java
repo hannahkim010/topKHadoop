83678551da4b37051ade51c09b88444cbb140759
hannahkim010 <92050051+hannahkim010@users.noreply.github.com>
2024-06-26 23:41:19 -0500

    Update WordCountReducer.java

--- src/main/java/edu/cs/utexas/HadoopEx/WordCountReducer.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCountReducer.java
@@ -2,21 +2,42 @@ package edu.cs.utexas.HadoopEx;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCountReducer extends  Reducer<Text, IntWritable, Text, IntWritable> {
+public class WordCountReducer extends  Reducer<Text, FloatWritable, Text, FloatWritable> {
 
-   public void reduce(Text text, Iterable<IntWritable> values, Context context)
+   public void reduce(Text text, Iterable<FloatWritable> values, Context context)
            throws IOException, InterruptedException {
-	   
-       int sum = 0;
+	
+    //    task 1 code:
+    //    int sum = 0;
        
-       for (IntWritable value : values) {
-           sum += value.get();
-       }
+    //    for (FloatWritable value : values) {
+    //        sum += value.get();
+    //    }
        
-       context.write(text, new IntWritable(sum));
+    //    context.write(text, new FloatWritable(sum));
+
+        // task 2 code:
+
+        // adjust to float 
+        float totalDelay = 0;
+        int flightCounter = 0;
+        
+        // for each map-key val for airline, sum up total delayAmts
+        for (FloatWritable value : values) {
+            totalDelay += value.get();
+            // increment flight counter
+            flightCounter += 1;
+        }
+
+        // calculates avg delay ratio given all the values
+        float avgDelayRatio = flightCounter > 0 ? totalDelay / flightCounter : 0;
+
+        // write in the delay ratio
+        // save airline with the found delay ratio
+        context.write(text, new FloatWritable(avgDelayRatio));
    }
 }
